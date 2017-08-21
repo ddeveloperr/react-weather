06b9a06d55796d5f0a1fbe8b9f49abae171dc167
KemalC <ddeveloperr@gmail.com>
2017-08-21 11:47:52 +0000

    Add Examples Component

--- public/bundle.js
+++ public/bundle.js
@@ -58,6 +58,7 @@
 	var Main = __webpack_require__(222);
 	var Weather = __webpack_require__(224);
 	var About = __webpack_require__(225);
+	var Examples = __webpack_require__(226);
 
 	ReactDOM.render(React.createElement(
 	  Router,
@@ -66,6 +67,7 @@
 	    Route,
 	    { path: '/', component: Main },
 	    React.createElement(Route, { path: 'about', component: About }),
+	    React.createElement(Route, { path: 'examples', component: Examples }),
 	    React.createElement(IndexRoute, { component: Weather })
 	  )
 	), document.getElementById('app'));
@@ -25483,7 +25485,12 @@
 	        { to: '/about' },
 	        'About'
 	      ),
-	      React.createElement(Link, null)
+	      ' |',
+	      React.createElement(
+	        Link,
+	        { to: '/examples' },
+	        'Examples'
+	      )
 	    );
 	  }
 	});
@@ -25531,6 +25538,27 @@
 	});
 
 	module.exports = About;
+
+/***/ }),
+/* 226 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	var React = __webpack_require__(1);
+	var Examples = React.createClass({
+	    displayName: 'Examples',
+
+	    render: function render() {
+	        return React.createElement(
+	            'h3',
+	            null,
+	            'Examples Component!'
+	        );
+	    }
+	});
+
+	module.exports = Examples;
 
 /***/ })
 /******/ ]);
