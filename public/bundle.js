f088220a1f80949fd3562960e6003b730399e9c1
KemalC <ddeveloperr@gmail.com>
2017-08-21 10:32:07 +0000

    Update & create Weather component

--- public/bundle.js
+++ public/bundle.js
@@ -56,11 +56,16 @@
 	    hashHistory = _require.hashHistory;
 
 	var Main = __webpack_require__(222);
+	var Weather = __webpack_require__(224);
 
 	ReactDOM.render(React.createElement(
 	  Router,
 	  { history: hashHistory },
-	  React.createElement(Route, { path: '/', component: Main })
+	  React.createElement(
+	    Route,
+	    { path: '/', component: Main },
+	    React.createElement(IndexRoute, { component: Weather })
+	  )
 	), document.getElementById('app'));
 
 /***/ }),
@@ -25434,7 +25439,8 @@
 	        'h2',
 	        null,
 	        'Main Component'
-	      )
+	      ),
+	      this.props.children
 	    );
 	  }
 	});
@@ -25462,6 +25468,27 @@
 	});
 
 	module.exports = Nav;
+
+/***/ }),
+/* 224 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	var React = __webpack_require__(1);
+	var Weather = React.createClass({
+	    displayName: 'Weather',
+
+	    render: function render() {
+	        return React.createElement(
+	            'h3',
+	            null,
+	            'Weather Component!'
+	        );
+	    }
+	});
+
+	module.exports = Weather;
 
 /***/ })
 /******/ ]);
