17f05cdb2a718b47729b2bfa52571c567c92f33d
KemalC <ddeveloperr@gmail.com>
2017-08-22 08:16:29 +0000

    Add and test input location from WForm to Weather component

--- public/bundle.js
+++ public/bundle.js
@@ -25511,6 +25511,10 @@
 	var Weather = React.createClass({
 	    displayName: 'Weather',
 
+
+	    handleSearch: function handleSearch(location) {
+	        alert(location);
+	    },
 	    render: function render() {
 	        return React.createElement(
 	            'div',
@@ -25520,7 +25524,7 @@
 	                null,
 	                'Weather Component!'
 	            ),
-	            React.createElement(WeatherForm, null),
+	            React.createElement(WeatherForm, { onSearch: this.handleSearch }),
 	            React.createElement(WeatherMessage, null)
 	        );
 	    }
@@ -25532,24 +25536,36 @@
 /* 225 */
 /***/ (function(module, exports, __webpack_require__) {
 
-	"use strict";
+	'use strict';
 
 	var React = __webpack_require__(1);
 	var WeatherForm = React.createClass({
-	    displayName: "WeatherForm",
+	    displayName: 'WeatherForm',
+
+
+	    onFormSubmit: function onFormSubmit(e) {
+	        e.preventDefault();
+
+	        var location = this.refs.location.value;
+
+	        if (location.length > 0) {
+	            this.refs.location.value = '';
+	            this.props.onSearch(location);
+	        }
+	    },
 
 	    render: function render() {
 	        return React.createElement(
-	            "div",
+	            'div',
 	            null,
 	            React.createElement(
-	                "form",
-	                null,
-	                React.createElement("input", { type: "text" }),
+	                'form',
+	                { onSubmit: this.onFormSubmit },
+	                React.createElement('input', { type: 'text', ref: 'location' }),
 	                React.createElement(
-	                    "button",
+	                    'button',
 	                    null,
-	                    "Get Weather"
+	                    'Get Weather'
 	                )
 	            )
 	        );
