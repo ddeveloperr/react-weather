0dc1d1ff3e28b60acf1b7c18eaa14c8233f42b20
KemalC <ddeveloperr@gmail.com>
2017-08-22 15:41:13 +0000

    play with debugger;

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -11,7 +11,9 @@ var Weather = React.createClass({
   },
   handleSearch: function (location) {
     var that = this;
-
+    
+    
+    
     this.setState({isLoading: true});
 
     openWeatherMap.getTemp(location).then(function (temp) {
