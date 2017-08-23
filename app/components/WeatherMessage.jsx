ef759b441b97035e7190ccf6ec955b13e3f73a05
KemalC <ddeveloperr@gmail.com>
2017-08-23 07:50:58 +0000

    update .gitignore

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,6 +1,5 @@
-var React              = require('react');
-
-var WeatherMessage     = ({location, temp}) => {
+var React          = require('react');
+var WeatherMessage = ({location, temp}) => {
 
   return (
       <h3>It's it {temp} in {location}.</h3>
